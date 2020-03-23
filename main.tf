49af777e795a4ba030e7ce4071a357c97a2c08f9
Maho Takara <takara9@gmail.com>
2020-03-23 07:54:36 +0000

    update

--- main.tf
+++ main.tf
@@ -8,63 +8,119 @@ variable "ic_api_key" {}
 ### クラスタレベルの環境設定変数
 ### OpenShift cluster env
 
-variable "domain" {}
-variable "datacenter" {}
-variable "sg1" {}
-variable "sg2" {}
+variable "domain" {
+  type = string
+  default = "example.com"
+}
+
+variable "datacenter" {
+  type = string
+  default = "tok05"
+}
+
+variable "sg1" {
+  type = string
+  default = "130379"
+  description = "allow_all"
+}
+
+variable "sg2" {
+  type = string
+  default = "130381"
+  description = "allow_outbound"
+}
 
 ### SSH鍵
-variable "ssh_user" {}
-variable "ssh_label" {}
-variable "ssh_notes" {}
+variable "ssh_user" {
+  type = string
+  default = "root"
+}
+variable "ssh_label" {
+  type = string
+  default = "tf-node"
+}
+variable "ssh_notes" {
+  type = string
+  default = "TFで作成したインスタンス用"
+}
 variable "ssh_key" {}
 
 ### OSなど共通事項
-variable "os_code" {}
-variable "private_only" {}
-
-
-### Mater node
-#variable "master" {
-#    num_of_node = "1"
-#    hostname = "master"
-#    vcpu = "2"
-#    ram  = "4"
-#    vol  = "25"
-#    nic  = "100"
-#    tag  = ""
-#}	 
-
-variable "master_node_num" {
-  type = list
+variable "os_code" {
+  type = string
+  default = "UBUNTU_16_64"
+}
+variable "private_only" {
+  type = string
+  default = "false"
 }
 
-variable "master_hostname" {}
-variable "master_vcpu" {}
-variable "master_ram" {}
-variable "master_disk_size" {}
-variable "master_nic_speed" {}
-variable "master_tags" {}
+variable "master_node" {
+  type = list(object({
+    node = number
+    name = string
+    vcpu = number
+    ram  = number
+    disk = number
+    nic  = number
+    tag  = string    
+  }))
+  default = [
+    {
+      node = 1
+      name = "master"
+      vcpu = 2
+      ram  = 4
+      disk = 25
+      nic  = 100
+      tag  = "test-deploy"
+    }
+  ]
+}
 
 ### Worker node
-variable "node_num" {}
-variable "node_hostname" {}
-variable "node_vcpu" {}
-variable "node_ram" {}
-variable "node_disk_size" {}
-variable "node_nic_speed" {}
-variable "node_tags" {}
+variable "worker_node" {
+  type = list(object({
+    node = number
+    name = string
+    vcpu = number
+    ram  = number
+    disk = number
+    nic  = number
+    tag  = string    
+  }))
+  default = [
+    {
+      node = 2
+      name = "node"
+      vcpu = 1
+      ram  = 1
+      disk = 25
+      nic  = 100
+      tag  = "test-deploy"
+    }
+  ]
+}
 
 #
 # IBM Cloud のAPIキーを変数ファイルから取得する
 #
 # https://ibm-cloud.github.io/tf-ibm-docs/index.html
 #
+#provider "ibm" {
+#  softlayer_username = "${var.sl_username}"
+#  softlayer_api_key  = "${var.sl_api_key}"
+#  bluemix_api_key    = "${var.ic_api_key}"
+#}
+
 provider "ibm" {
-  softlayer_username = "${var.sl_username}"
-  softlayer_api_key  = "${var.sl_api_key}"
-  bluemix_api_key    = "${var.ic_api_key}"
+  #ibmcloud_api_key    = "${var.ibmcloud_api_key}"
+  generation = 1
+  region = "jp-tokyo"
+  iaas_classic_username = "${var.sl_username}"
+  iaas_classic_api_key  = "${var.sl_api_key}"
 }
+
 
 #
 # OpenShiftクラスタ共通のssh鍵を設定する
@@ -82,46 +138,47 @@ resource "ibm_compute_ssh_key" "ssh_key" {
 # https://ibm-cloud.github.io/tf-ibm-docs/v0.14.1/r/compute_vm_instance.html
 #
 resource "ibm_compute_vm_instance" "master" {
-  count                    = "${var.num_of_master}"
-  hostname                 = "${var.hostname}-${count.index}"
+  count                    = "${var.master_node[node]}"
+  hostname                 = "${var.master_node[name]}-${count.index}"
   os_reference_code        = "${var.os_code}"
   domain                   = "${var.domain}"
   datacenter               = "${var.datacenter}"
-  network_speed            = "${var.nic_speed}"
+  network_speed            = "${var.master_node[nic]}"
   hourly_billing           = true
   private_network_only     = "${var.private_only}"
-  cores                    = "${var.vcpu}"
-  memory                   = "${var.ram}"
-  disks                    = ["${var.disk_size}"]
+  cores                    = "${var.master_node[vcpu]}"
+  memory                   = "${var.master_node[ram]}"
+  disks                    = ["${var.master_node[disk]}"]
   dedicated_acct_host_only = false
   local_disk               = false
   ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
-  tags                     = ["${var.tags}"]
+  tags                     = ["${var.master_node[tags]}"]
   user_metadata            = "${file("install.yml")}"
   private_security_group_ids = ["${var.sg1}","${var.sg2}"]
   public_security_group_ids  = ["${var.sg1}","${var.sg2}"]
 }
 
 resource "ibm_compute_vm_instance" "node" {
-  count                    = "${var.num_of_node}"
-  hostname                 = "${var.hostname}-${count.index}"
+  count                    = "${var.worker_node[node]}"
+  hostname                 = "${var.worker_node[name]}-${count.index}"
   os_reference_code        = "${var.os_code}"
   domain                   = "${var.domain}"
   datacenter               = "${var.datacenter}"
-  network_speed            = "${var.nic_speed}"
+  network_speed            = "${var.worker_node[nic]}"
   hourly_billing           = true
   private_network_only     = "${var.private_only}"
-  cores                    = "${var.vcpu}"
-  memory                   = "${var.ram}"
-  disks                    = ["${var.disk_size}"]
+  cores                    = "${var.worker_node[vcpu]}"
+  memory                   = "${var.worker_node[ram]}"
+  disks                    = ["${var.worker_node[disk]}"]
   dedicated_acct_host_only = false
   local_disk               = false
   ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
-  tags                     = ["${var.tags}"]
+  tags                     = ["${var.worker_node[tags]}"]
   user_metadata            = "${file("install.yml")}"
   private_security_group_ids = ["${var.sg1}","${var.sg2}"]
   public_security_group_ids  = ["${var.sg1}","${var.sg2}"]
 }
+
 
 
 output "public_ip_master" {
--- terraform.tfvars
+++ terraform.tfvars
@@ -6,22 +6,22 @@
 #space_name  = ""
 
 ## ホストとドメイン名
-hostname   = "node"
-domain     = "sample.org"
+#hostname   = "node"
+#domain     = "sample.org"
 
 ## ノード数
-num_of_master = "1"
-num_of_node   = "2"
+#num_of_master = "1"
+#num_of_node   = "2"
 
 ## bx sl vs options
-datacenter = "tok05"
-os_code    = "UBUNTU_16_64"
-vcpu       = "1"
-ram        = "1024"
-disk_size  = "25"
-private_only = "false"
-nic_speed  = "100"
-tags       = "TF"
+#datacenter = "tok05"
+#os_code    = "UBUNTU_16_64"
+#vcpu       = "1"
+#ram        = "1024"
+#disk_size  = "25"
+#private_only = "false"
+#nic_speed  = "100"
+#tags       = "TF"
 
 ##
 ssh_user   = "root"
@@ -31,8 +31,8 @@ ssh_notes  = "Terraform ssh-key"
 
 ## セキュリティ・グループのIDで Public VLAN とPrivate VLAN のアクセス制御を設定
 # bx sl securitygroup list
-sg1 = "130379" # allow_all
-sg2 = "130381" # allow_outbound
+#sg1 = "130379" # allow_all
+#sg2 = "130381" # allow_outbound
 
 
 
