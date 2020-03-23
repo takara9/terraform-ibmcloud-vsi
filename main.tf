3e245db2b27733f0b303069eac160c219a84804c
Maho Takara <takara9@gmail.com>
2020-03-23 13:54:26 +0000

    update

--- main.tf
+++ main.tf
@@ -48,77 +48,98 @@ variable "ssh_key" {}
 ### OSなど共通事項
 variable "os_code" {
   type = string
-  default = "UBUNTU_16_64"
+  default = "CENTOS_7_64"
 }
 variable "private_only" {
   type = string
   default = "false"
 }
 
-variable "master_node" {
-  type = list(object({
-    node = number
-    name = string
-    vcpu = number
-    ram  = number
-    disk = number
-    nic  = number
-    tag  = string    
-  }))
-  default = [
-    {
-      node = 1
-      name = "master"
-      vcpu = 2
-      ram  = 4
-      disk = 25
-      nic  = 100
-      tag  = "test-deploy"
-    }
-  ]
+variable "master_node_num" {
+  type = number
+  default = 1
 }
 
-### Worker node
-variable "worker_node" {
-  type = list(object({
-    node = number
-    name = string
-    vcpu = number
-    ram  = number
-    disk = number
-    nic  = number
-    tag  = string    
-  }))
-  default = [
-    {
-      node = 2
-      name = "node"
-      vcpu = 1
-      ram  = 1
-      disk = 25
-      nic  = 100
-      tag  = "test-deploy"
-    }
-  ]
+variable "master_node_name" {
+  type = string
+  default = "master"
 }
+
+variable "master_node_vcpu" {
+  type = number
+  default = 2
+}
+
+variable "master_node_ram" {
+  type = number
+  default = 4096
+}
+
+variable "master_node_disk" {
+  type = number
+  default = 25
+}
+
+variable "master_node_nic" {
+  type = number
+  default = 100
+}
+
+variable "master_node_tags" {
+  type = string
+  default = "ocp-test"
+}
+
+
+
+variable "worker_node_num" {
+  type = number
+  default = 2
+}
+
+variable "worker_node_name" {
+  type = string
+  default = "node"
+}
+
+variable "worker_node_vcpu" {
+  type = number
+  default = 1
+}
+
+variable "worker_node_ram" {
+  type = number
+  default = 1024
+}
+
+variable "worker_node_disk" {
+  type = number
+  default = 25
+}
+
+variable "worker_node_nic" {
+  type = number
+  default = 100
+}
+
+variable "worker_node_tags" {
+  type = string
+  default = "ocp-test"
+}
+
 
 #
 # IBM Cloud のAPIキーを変数ファイルから取得する
 #
 # https://ibm-cloud.github.io/tf-ibm-docs/index.html
 #
-#provider "ibm" {
-#  softlayer_username = "${var.sl_username}"
-#  softlayer_api_key  = "${var.sl_api_key}"
-#  bluemix_api_key    = "${var.ic_api_key}"
-#}
 
 provider "ibm" {
-  #ibmcloud_api_key    = "${var.ibmcloud_api_key}"
+  #ibmcloud_api_key    = var.ibmcloud_api_key
   generation = 1
   region = "jp-tokyo"
-  iaas_classic_username = "${var.sl_username}"
-  iaas_classic_api_key  = "${var.sl_api_key}"
+  iaas_classic_username = var.sl_username
+  iaas_classic_api_key  = var.sl_api_key
 }
 
 
@@ -128,78 +149,82 @@ provider "ibm" {
 # https://ibm-cloud.github.io/tf-ibm-docs/v0.14.1/d/compute_ssh_key.html
 #
 resource "ibm_compute_ssh_key" "ssh_key" {
-  label      = "${var.ssh_label}"
-  notes      = "${var.ssh_notes}"
-  #public_key = "${var.ssh_key}"
-  public_key = "${file("/home/vagrant/keys/key.pub")}"
+  label      = var.ssh_label
+  notes      = var.ssh_notes
+  #public_key = var.ssh_key
+  public_key = file("/home/vagrant/keys/key.pub")
 }
 
 
 # https://ibm-cloud.github.io/tf-ibm-docs/v0.14.1/r/compute_vm_instance.html
 #
 resource "ibm_compute_vm_instance" "master" {
-  count                    = "${var.master_node[node]}"
-  hostname                 = "${var.master_node[name]}-${count.index}"
-  os_reference_code        = "${var.os_code}"
-  domain                   = "${var.domain}"
-  datacenter               = "${var.datacenter}"
-  network_speed            = "${var.master_node[nic]}"
+  count                    = var.master_node_num
+  hostname                 = "${var.master_node_name}${count.index}"
+  os_reference_code        = var.os_code
+  domain                   = var.domain
+  datacenter               = var.datacenter
+  network_speed            = var.master_node_nic
   hourly_billing           = true
-  private_network_only     = "${var.private_only}"
-  cores                    = "${var.master_node[vcpu]}"
-  memory                   = "${var.master_node[ram]}"
-  disks                    = ["${var.master_node[disk]}"]
+  private_network_only     = var.private_only
+  cores                    = var.master_node_vcpu
+  memory                   = var.master_node_ram
+  disks                    = [var.master_node_disk]
   dedicated_acct_host_only = false
   local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
-  tags                     = ["${var.master_node[tags]}"]
-  user_metadata            = "${file("install.yml")}"
-  private_security_group_ids = ["${var.sg1}","${var.sg2}"]
-  public_security_group_ids  = ["${var.sg1}","${var.sg2}"]
+  ssh_key_ids              = [ibm_compute_ssh_key.ssh_key.id]
+  tags                     = [var.master_node_tags]
+  user_metadata            = file("install.yml")
+  private_security_group_ids = [var.sg1,var.sg2]
+  public_security_group_ids  = [var.sg1,var.sg2]
 }
 
 resource "ibm_compute_vm_instance" "node" {
-  count                    = "${var.worker_node[node]}"
-  hostname                 = "${var.worker_node[name]}-${count.index}"
-  os_reference_code        = "${var.os_code}"
-  domain                   = "${var.domain}"
-  datacenter               = "${var.datacenter}"
-  network_speed            = "${var.worker_node[nic]}"
+  count                    = var.worker_node_num
+  hostname                 = "${var.worker_node_name}${count.index}"
+  os_reference_code        = var.os_code
+  domain                   = var.domain
+  datacenter               = var.datacenter
+  network_speed            = var.worker_node_nic
   hourly_billing           = true
-  private_network_only     = "${var.private_only}"
-  cores                    = "${var.worker_node[vcpu]}"
-  memory                   = "${var.worker_node[ram]}"
-  disks                    = ["${var.worker_node[disk]}"]
+  private_network_only     = var.private_only
+  cores                    = var.worker_node_vcpu
+  memory                   = var.worker_node_ram
+  disks                    = [var.worker_node_disk]
   dedicated_acct_host_only = false
   local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
-  tags                     = ["${var.worker_node[tags]}"]
-  user_metadata            = "${file("install.yml")}"
-  private_security_group_ids = ["${var.sg1}","${var.sg2}"]
-  public_security_group_ids  = ["${var.sg1}","${var.sg2}"]
+  ssh_key_ids              = [ibm_compute_ssh_key.ssh_key.id]
+  tags                     = [var.worker_node_tags]
+  user_metadata            = file("install.yml")
+  private_security_group_ids = [var.sg1,var.sg2]
+  public_security_group_ids  = [var.sg1,var.sg2]
 }
 
 
 
 output "public_ip_master" {
-  value = "${ibm_compute_vm_instance.master.*.ipv4_address}"
+  value = ibm_compute_vm_instance.master.*.ipv4_address
+}
+output "private_ip_master" {
+  value = ibm_compute_vm_instance.node.*.ipv4_address_private
 }
 
+
 output "public_ip_nodes" {
-  value = "${ibm_compute_vm_instance.node.*.ipv4_address}"
+  value = ibm_compute_vm_instance.node.*.ipv4_address
+}
+output "private_ip_nodes" {
+  value = ibm_compute_vm_instance.node.*.ipv4_address_private
 }
 
 
 output "hostname_master" {
-  value = "${ibm_compute_vm_instance.master.*.hostname}"
+  value = ibm_compute_vm_instance.master.*.hostname
 }
-
 output "hostname_nodes" {
-  value = "${ibm_compute_vm_instance.node.*.hostname}"
+  value = ibm_compute_vm_instance.node.*.hostname
 }
 
-output "base_name" {
-  value = "${var.hostname}"
-}
+
 
 
--- tfstate2hosts.py
+++ tfstate2hosts.py
@@ -7,44 +7,47 @@ import shutil
 ## teraform.tfstate の output を読んで、Ansible の Inventoryファイルを作る
 buf = open('terraform.tfstate').read()
 dic = json.loads(buf)
-m = dic['modules'][0]
+m = dic['outputs']
 
 hostname = []
 public_ip = []
 lines = []
 
-for x in m['outputs']['hostname']['value']:
+for x in m['hostname_master']['value']:
     hostname.append(x)
 
-for x in m['outputs']['public_ip']['value']:    
+for x in m['public_ip_master']['value']:    
     public_ip.append(x)
 
+for x in m['hostname_nodes']['value']:
+    hostname.append(x)
+
+for x in m['public_ip_nodes']['value']:    
+    public_ip.append(x)
     
+
     
+## 全インベントリのリスト作成
 priv_key = '~/keys/key'
 form = '{0} ansible_ssh_host={1} ansible_ssh_private_key_file={2} ansible_ssh_user="root"\n'    
-n_of_host = len(hostname)
-#base_name = 'tkr01tf-'
-base_name = m['outputs']['base_name']['value'] + '-'
-
-
-## ノードデータ作成
-for i in range(0,n_of_host):
+for i in range(0,len(hostname)):
     line = form.format(hostname[i], public_ip[i], priv_key)
     lines.append(line)
 
-
+    
 ## インベントリファイルのバックアップ    
 src = './playbooks/hosts'
 dst = './playbooks/hosts.backup'
 shutil.copyfile(src,dst)
 
-
 ## Terraformから起動したノードのエントリを追加
-with open(src, mode='a') as f:
+with open(src, mode='w') as f:
     for l in lines:
         f.write(l)
+    f.write('[masters]\n')
+    f.write('{0}[0:{1}]'.format("master",len(m['hostname_master']['value'])-1))
+    f.write('\n')    
     f.write('[nodes]\n')
-    f.write('{0}[0:{1}]'.format(base_name,n_of_host-1))
-
+    f.write('{0}[0:{1}]'.format("node",len(m['hostname_nodes']['value'])-1))
+    f.write('\n')    
 
